c8f1d9b42b2cc51ce6f5d9e1845f8fe0864c7849
Alan Dominguez <alandominguez122@gmail.com>
2024-10-26 15:02:24 -0300

    Ejercicios practica 6

--- Ejerp6.R
+++ Ejerp6.R
@@ -14,15 +14,20 @@ plot (x, densidad, type = "l" , xlab = "X", ylab = "Densidad", col = "blue")
 qgamma (.75, shape = alpha, scale = 1/lambda)
 
 #c)
-R <- 10000
-n <- 100
+R <- 800
+n <- 25
 
-cuantil <- rep(0,R)
+cuantiles_75 <- rep(0,R)
 set.seed(34)
 
 for(i in 1:R) {
-cuantil[i] <- quantile( rgamma(n, shape = alpha, scale = 1/lambda), .75)
+cuantiles_75[i] <- quantile( rgamma(n, shape = alpha, scale = 1/lambda), .75)
 }
 
-mean(cuantil)
-hist(cuantil, ylab="Frecuencia")
+mean(cuantiles_75)
+hist(cuantiles_75, ylab="Frecuencia")
+
+#e)
+errorEstandar <- sd(cuantiles_75);
+errorEstandar
+cat("El error estandar es de ", errorEstandar, " min y, dado que la media es de ", mean(cuantiles_75), "min, representa un 14,7% por lo tanto el error sigue siendo una fracción significativa de tita sombrero")
